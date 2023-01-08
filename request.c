cb5403537157e984cb3d118360785bcbe288d824
KIM JINJAE <jinjae.dev@gmail.com>
2023-01-08 20:29:13 +0900

    refactor: separate function

--- request.c
+++ request.c
@@ -6,45 +6,48 @@
 
 void    requestController(int *localSocket, char *address, char *hierarchy)
 {
+    // message 문자열을 생성합니다.
     char	message[BUFFER_SIZE] = "";
     int	sendClientSocket = *(int *) localSocket;
 
-    strcat(message, GET);
-    strcat(message, " ");
-    strcat(message, "/");
-    if (hierarchy != NULL)
-        strcat(message, hierarchy);
-    strcat(message, " ");
-    strcat(message, HTTP_VERSION);
-    strcat(message, " ");
-    strcat(message, END_LINE);
+    // 요청에 맞는 메시지를 생성합니다.
+    createMessage(message, address, hierarchy);
 
-    strcat(message, "Host: ");
-    strcat(message, address);
-    strcat(message, END_LINE);
-
-    strcat(message, "Connection: ");
-    strcat(message, KEEP_ALIVE);
-    strcat(message, END_LINE);
-
-    strcat(message, "User-Agent: ");
-    strcat(message, MY_AGENT);
-    strcat(message, END_LINE);
-
-    strcat(message, "Accept: ");
-    strcat(message, ACCEPT);
-    strcat(message, END_LINE);
-
-    strcat(message, "Accept-Language: ");
-    strcat(message, ACCEPT_LANGUAGE);
-    strcat(message, END_LINE);
-
-    strcat(message, END_LINE);
-
-    printf("%s", message);
-
+    // server 에 해당 요청을 전송합니다.
     if (send(sendClientSocket, message, BUFFER_SIZE - 1, 0) <= 0)
         throwException("SEND_ERR");
+}
+
+void    createMessage(char *message, char *address, char *hierarchy)
+{
+    // client 에서 요청할 때 login.html 을 조회할 때에는 POST 요청, 아닌 경우에는 GET 요청을 하는 것으로 가정하였습니다.
+    if (hierarchy != NULL && !strcmp(hierarchy, "login.html"))
+    {
+        // POST /login.html
+        strcat(strcat(message, POST), " ");
+        strcat(strcat(message, "/"), hierarchy);
+        strcat(message, " ");
+    }
+    else
+    {
+        // GET /{hierarchy}
+        strcat(strcat(message, GET), " ");
+        strcat(message, "/");
+        if (hierarchy != NULL)
+            strcat(message, hierarchy);
+        strcat(message, " ");
+    }
+
+    // HTTP/1.1
+    strcat(strcat(message, HTTP_VERSION), END_LINE);
+    // 각종 헤더들입니다.
+    strcat(strcat(strcat(message, "Host: "), address), END_LINE);
+    strcat(strcat(strcat(message, "User-Agent: "), MY_AGENT), END_LINE);
+    strcat(strcat(strcat(message, "Accept: "), ACCEPT), END_LINE);
+    strcat(strcat(strcat(message, "Accept-Language: "), ACCEPT_LANGUAGE), END_LINE);
+
+    // 헤더의 끝을 알립니다.
+    strcat(message, END_LINE);
 }
 
 void    receiveResponse(int *localSocket)
@@ -52,6 +55,7 @@ void    receiveResponse(int *localSocket)
     int	sendClientSocket = *(int *) localSocket;
     char html[BUFFER_SIZE];
 
+    // 서버로부터 response 메시지를 받아서 출력합니다.
     recv(sendClientSocket, html, BUFFER_SIZE - 1, 0);
     printf("%s\n", html);
 }
--- request.h
+++ request.h
@@ -7,15 +7,17 @@
 
 #include "library.h"
 
+// 헤더에 들어가는 각종 내용들을 매크로로 정의하였습니다.
 #define GET "GET"
+#define POST "POST"
 #define HTTP_VERSION "HTTP/1.1"
 #define END_LINE    "\r\n"
-#define KEEP_ALIVE "keep-alive"
 #define MY_AGENT  "curl"
 #define ACCEPT  "text/html"
 #define ACCEPT_LANGUAGE "ko,en-US;q=0.9,en;q=0.8,ko-KR;q=0.7,ja;q=0.6"
 
 void    requestController(int *localSocket, char *address, char *hierarchy);
+void    createMessage(char *message, char *address, char *hierarchy);
 void    receiveResponse(int *localSocket);
 
 #endif //SOCKET_CLIENT_REQUEST_H
